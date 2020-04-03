67a6a60cc48a90abea07e336b97a0855ef54e304
cybersrikanth <ksksrikanthksk@gmail.com>
2020-04-04 02:49:29 +0530

    added password val

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const {validateText} = require('./validators/validateText')
 const {validateNumberLength} = require('./validators/validateNumberLength')
 const {validateBool} = require('./validators/validateBool')
 const {validateAddress} = require('./validators/validateAddress')
+const {validatePassword} = require('./validators/validatePassword')
 const {filter} = require('./filter')
 
-exports = {
-    validateEmail, validateString,validateText, validateNumberLength, validateBool, validateAddress, filter
-}
+exports.validateEmail = validateEmail;
+exports.validateString = validateString;
+exports.validatePassword = validatePassword;
+exports.validateText = validateText;
+exports.validateNumberLength = validateNumberLength;
+exports.validateBool = validateBool;
+exports.validateAddress = validateAddress;
+exports.filter = filter
