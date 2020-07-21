fb009b41f146d4cc8e9c17fe388af168dca05636
antorichard <anto22998@gmail.com>
2020-07-22 00:42:53 +0530

    exported as module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import Validator from "./validators/Validator";
 
-try {
-    const user = new Validator(123);
-    user.isLength(4).isEmpty();    
-} catch (error) {
-    console.log(error);
-}
-
+export default Validator;
