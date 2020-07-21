9ba50aa3dd53e9f9b19206c3f81da195cc785aa4
antorichard <anto22998@gmail.com>
2020-07-22 00:45:47 +0530

    code formatted

--- src/models/validator.ts
+++ src/models/validator.ts
@@ -2,4 +2,7 @@ export default interface ErrorType {
     isLength(length: number): this;
     max(limit: number): this;
     min(limit: number): this;
+    isNumber(): this;
+    isAlpha(): this;
+    isEmpty(): this;
 }
