78837f2c05a9452bfb05df5e8666d52bbf645743
antorichard <anto22998@gmail.com>
2020-07-22 22:42:15 +0530

    added path to email validation in index file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 import Email from "./validators/Email";
 
-const email = new Email("Test@test.com");
-
-try {
-    const result = email.sanitize();
-    console.log(result)
-} catch (error) {
-    console.log(error)
-}
+module.exports = { Email }
